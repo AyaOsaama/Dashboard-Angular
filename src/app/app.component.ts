e7a7adbf10005ce84ccad7dcf521948814238edc
MohammedKabil2021 <Mohamedkabil54@gmail.com>
2025-05-01 04:08:34 +0300

    finished my task products-list page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,133 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TableModule } from 'primeng/table';
-import { TagModule } from 'primeng/tag';
-import { RatingModule } from 'primeng/rating';
-import { CommonModule } from '@angular/common';
-import { ButtonModule } from 'primeng/button';
-import { FormsModule } from '@angular/forms';
-import { CarouselModule } from 'primeng/carousel';
+import { ProductsListComponent } from "./products/pages/products-list/products-list/products-list.component";
+
+// import { RouterOutlet } from '@angular/router';
+// import { TableModule } from 'primeng/table';
+// import { TagModule } from 'primeng/tag';
+// import { RatingModule } from 'primeng/rating';
+// import { CommonModule } from '@angular/common';
+// import { ButtonModule } from 'primeng/button';
+// import { FormsModule } from '@angular/forms';
+// import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    ButtonModule,
-    TableModule,
-    TagModule,
-    RatingModule,
-    ButtonModule,
-    CommonModule,
-    FormsModule,
-    CarouselModule,
+    ProductsListComponent
+    // ButtonModule,
+    // TableModule,
+    // TagModule,
+    // RatingModule,
+    // ButtonModule,
+    // CommonModule,
+    // FormsModule,
+    // CarouselModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'Dashboard';
-  products = [
-    {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Bamboo Watch',
-      description: 'Product Description',
-      image: 'bamboo-watch.jpg',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5,
-    },
-    {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Bamboo Watch',
-      description: 'Product Description',
-      image: 'bamboo-watch.jpg',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5,
-    },
-    {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Bamboo Watch',
-      description: 'Product Description',
-      image: 'bamboo-watch.jpg',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5,
-    },
-    {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Bamboo Watch',
-      description: 'Product Description',
-      image: 'bamboo-watch.jpg',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5,
-    },
-    {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Bamboo Watch',
-      description: 'Product Description',
-      image: 'bamboo-watch.jpg',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5,
-    },
-    {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Bamboo Watch',
-      description: 'Product Description',
-      image: 'bamboo-watch.jpg',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5,
-    },
-  ];
-  responsiveOptions: any[] | undefined;
+export class AppComponent {
 
-  ngOnInit() {
-    this.responsiveOptions = [
-      {
-        breakpoint: '1400px',
-        numVisible: 2,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '1199px',
-        numVisible: 3,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '767px',
-        numVisible: 2,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '575px',
-        numVisible: 1,
-        numScroll: 1,
-      },
-    ];
-  }
-  getSeverity(status: string): 'success' | 'info' | 'danger' | 'warn' {
-    // your logic
-    return 'warn'; // instead of 'warning'
-  }
 }
