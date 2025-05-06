3fed8c53e5271942e5ff644e8902a574c68bd37f
AyaOsaama <ayaosama40a@gmail.com>
2025-05-06 21:16:38 +0300

    change apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: false,
     // apiUrl: 'https://furniture-backend-tmsay1ef0-ayaosaamas-projects.vercel.app'
-    apiUrl:'http://localhost:3000'
+    // apiUrl:'http://localhost:3000'
+    apiUrl:'https://furniture-backend-production-8726.up.railway.app/'
+
   };
   
