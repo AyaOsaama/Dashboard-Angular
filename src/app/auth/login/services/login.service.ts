8c1e0f5bda722f41f60aaa4754428d952e8f39f1
AyaOsaama <ayaosama40a@gmail.com>
2025-05-08 16:35:30 +0300

    edit login service

--- src/app/auth/login/services/login.service.ts
+++ src/app/auth/login/services/login.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private baseUrl = 'https://furniture-backend-production-8726.up.railway.app';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
