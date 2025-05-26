c9ec414cc20f86523796801b92e9c6e5561be10b
Nada Ossama <118224738+NadaOssama2001@users.noreply.github.com>
2025-05-26 19:23:10 +0300

    edit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: true,
     // apiUrl:'https://furniture-backend-tmsay1ef0-ayaosaamas-projects.vercel.app'
     // apiUrl:'https://furniture-backend-production-8726.up.railway.app'
-       apiUrl:'http://localhost:3000'
+    //    apiUrl:'http://localhost:3000'
+    apiUrl:'https://furniture-nodejs-production-665a.up.railway.app'
  };
   
